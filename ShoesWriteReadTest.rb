b6d0f2bbe872913dedd74c7964133a9a1bf45427
morgy <morgy@Queeg500.local>
2009-04-25 15:30:14 +0100

    *Modified, removed tabs to simplify example

--- ShoesWriteReadTest.rb
+++ ShoesWriteReadTest.rb
@@ -27,8 +27,6 @@ require 'yaml'
    
 class CanonicalConversion < Shoes
    url '/',      :index
-   url '/pref',  :pref
-   url '/about', :about
 
    $configFile = ".amaras/RWTestConfig.yml"
    $numbers = {} 
@@ -54,7 +52,8 @@ class CanonicalConversion < Shoes
    
    def index
       @app = app
-      topMenu(1)
+      #Reload Config file on every tab change
+      @config = loadConfig($configFile)
       
       stack {
          flow { 
@@ -75,93 +74,6 @@ class CanonicalConversion < Shoes
       }
       
    end
-   
-   def about
-      @app = app
-      topMenu(0)
-      stack {
-         para "Testing Packaged Shoes File Read Write" 
-         para "Acknowledgements" 
-         para "Copyright" 
-      }
-   end
-   
-   def pref
-      @app = app
-      topMenu(2)
-      stack {
-         para "No Prefs"
-      }
       
-   end 
-   
-   def topMenu (tabNo)
-      
-      #Change settings here as required
-      inactiveTabColor = "#B0B0B0"
-      mainBodyColor    = "#D0D0D0"
-      topColor         = "#E0E0E0"
-      noOfTabs         = 3
-      tabWidth         = 60
-      tabHeight        = 30
-      tabOverlay       = 8
-      ##################################
-      #Order and links of tabs
-      names = Array.new
-      names[0] = {:name=>"about", :link=>"/about"}
-      names[1] = {:name=>"main",  :link=>"/"}
-      names[2] = {:name=>"prefs", :link=>"/pref"}
-      ##################################
-      
-      background mainBodyColor
-      #Make background for tabs a lighter colour
-      background (topColor, :height=> (tabHeight-tabOverlay))
-
-      #Add black line under tabs
-      stroke black
-      @app.line(0,(tabHeight-tabOverlay),@app.width(),(tabHeight-tabOverlay))
-      
-      #Do not alter calculations for tabs
-      tabsSpace = @app.width() / (noOfTabs + 1)
-      currentTabCentre = tabsSpace
-      indentamount = Array.new
-      #Loop and draw tabs
-      (0... noOfTabs).each do |i|
-         #Deactive tabs different colour
-         if i == tabNo
-            fill mainBodyColor
-         else 
-            fill inactiveTabColor
-         end
-         #Calculate position and size of tabs
-         currentTabStart = currentTabCentre - (tabWidth/2)
-         indentamount[i]  = currentTabStart
-         @app.rect(currentTabStart,2,tabWidth, (tabHeight-2+5), :curve=>12)
-         currentTabCentre  = currentTabCentre + tabsSpace
-         
-         #Add black line to the bottom of deactivated tabs
-         if i != tabNo
-            @app.line(currentTabStart,(tabHeight-8),(currentTabStart+tabWidth),(tabHeight-8))
-         end
-         
-         #Set Tab Text Here
-         @app.flow (:left => indentamount[i], :width => tabWidth) { 
-           para (link(names[i][:name], :click=>names[i][:link],  :stroke => black, :underline => "none"), :align => "center")
-         }
-         
-      end
-      
-      #Cover up the bottom of the tabs so they have square bottom
-      fill   mainBodyColor
-      stroke mainBodyColor
-      @app.rect(0, (tabHeight-tabOverlay+1), @app.width(), (tabOverlay+4))
-   
-      #Put pen back to black
-      stroke black
-      
-      #Reload Config file on every tab change
-      @config = loadConfig($configFile)
-   end
-   
 end
 Shoes.app :title => "Shoes Write Read Test",  :width=>400, :height=>500
